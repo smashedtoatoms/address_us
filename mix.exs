63418a8347c232cc797c335932a633a0a8ec3eb1
Jason Legler <j@legler.io>
2024-05-10 10:33:58 -0500

    fix: updated deps, build/publish of docs, version
    I incremented the version, updated dependencies, and set a version in
    asdf so that I could publish.  The latest version of elixir/erlang
    appears to be unable to publish to hex.pm, so I pinned the version with
    asdf for now.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AddressUS.Mixfile do
   def project do
     [
       app: :address_us,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: ">= 1.6.0",
       name: "AddressUS",
       source_url: "https://github.com/smashedtoatoms/address_us",
@@ -15,26 +15,13 @@ defmodule AddressUS.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:earmark, "~> 1.4", only: :dev},
-      {:ex_doc, "~> 0.23.0", only: :dev}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
     ]
   end
 
@@ -48,10 +35,9 @@ defmodule AddressUS.Mixfile do
     [
       files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Jason Legler"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => "https://github.com/smashedtoatoms/address_us",
-        "Docs" => "https://smashedtoatoms.github.io/address_us"
       }
     ]
   end
